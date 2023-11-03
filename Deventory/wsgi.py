e46dc11d8a2013d35a2e2b7271347d3a0cf080ec
Suryansh Shakya <83297944+nullHawk@users.noreply.github.com>
2023-11-03 21:55:03 +0530

    django boiler plate

---
+++ Deventory/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Deventory project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Deventory.settings')
+
+application = get_wsgi_application()
